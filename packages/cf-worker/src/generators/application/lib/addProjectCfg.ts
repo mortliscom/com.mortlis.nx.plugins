fdd3844d1bf4f563fde70f85369ec62561d4436a
Elias Cerne <eliascerne@icloud.com>
2022-10-13 22:13:37 +0200

    feat: rename addProjectCfg

---
+++ packages/cf-worker/src/generators/application/lib/addProjectCfg.ts
@@ -0,0 +1,65 @@
+import { NormalisedSchema } from './normaliseOptions';
+import {
+  addProjectConfiguration,
+  joinPathFragments,
+  ProjectConfiguration,
+  Tree,
+} from '@nrwl/devkit';
+
+export function addProjectCfg(host: Tree, options: NormalisedSchema) {
+  const targets: Record<string, any> = {};
+
+  targets.build = {
+    builder: '@nrwl/next:build',
+    outputs: ['{options.outputPath}'],
+    defaultConfiguration: 'production',
+    options: {
+      root: options.projectRoot,
+      outputPath: joinPathFragments('dist', options.projectRoot),
+    },
+    configurations: {
+      development: {
+        outputPath: options.projectRoot,
+      },
+      production: {},
+    },
+  };
+
+  targets.serve = {
+    builder: '@nrwl/next:server',
+    defaultConfiguration: 'development',
+    options: {
+      buildTarget: `${options.projectName}:build`,
+      dev: true,
+    },
+    configurations: {
+      development: {
+        buildTarget: `${options.projectName}:build:development`,
+        dev: true,
+      },
+      production: {
+        buildTarget: `${options.projectName}:build:production`,
+        dev: false,
+      },
+    },
+  };
+
+  targets.export = {
+    builder: '@nrwl/next:export',
+    options: {
+      buildTarget: `${options.projectName}:build:production`,
+    },
+  };
+
+  const project: ProjectConfiguration = {
+    root: options.projectRoot,
+    sourceRoot: `${options.projectRoot}/src`,
+    projectType: 'application',
+    targets,
+    tags: options.parsedTags,
+  };
+
+  addProjectConfiguration(host, options.projectName, {
+    ...project,
+  });
+}
