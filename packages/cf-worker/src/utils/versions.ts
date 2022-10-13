d5a2d662159aa3d44f6e620e92bbd09cec8595e9
Elias Cerne <eliascerne@icloud.com>
2022-10-13 21:04:25 +0200

    feat: add versions for init

---
+++ packages/cf-worker/src/utils/versions.ts
@@ -0,0 +1,7 @@
+export const nxVersion = require('../../package.json').version;
+
+export const nextVersion = '12.3.1';
+export const wranglerVersion = '2.1.11';
+export const eslintConfigNextVersion = '12.3.1';
+export const babelPluginStyledComponentsVersion = '1.10.7';
+export const tsLibVersion = '^2.3.0';
