97ecf8cf03e86042223922ab5d76534209fea093
Elias Cerne <eliascerne@icloud.com>
2022-10-13 21:21:14 +0200

    feat: add function to init and return run Tasks in serial

--- packages/cf-worker/src/generators/init/init.ts
+++ packages/cf-worker/src/generators/init/init.ts
@@ -9,16 +9,23 @@ import {
   offsetFromRoot,
   Tree,
 } from '@nrwl/devkit';
+import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
 import * as path from 'path';
 import { InitSchema } from './schema';
 import { updateDependencies } from './lib/updateDependencies';
+import { addGitIgnoreEntry } from '../../utils/addGitIgnoreEntry';
 
-interface NormalizedSchema extends InitSchema {}
+interface NormalisedSchema extends InitSchema {}
 
 export async function InitGenerator(host: Tree, schema: InitSchema) {
   const tasks: GeneratorCallback[] = [];
 
   const installTask = updateDependencies(host);
+  tasks.push(installTask);
+
+  addGitIgnoreEntry(host);
+
+  return runTasksInSerial(...tasks);
 }
 
 export default InitGenerator;
