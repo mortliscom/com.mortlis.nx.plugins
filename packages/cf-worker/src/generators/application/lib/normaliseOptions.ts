21320884ea23b04987a636271d9378aa5ba5e4d6
Elias Cerne <eliascerne@icloud.com>
2022-10-13 20:32:01 +0200

    feat: change normaliseOptions

--- packages/cf-worker/src/generators/application/lib/normaliseOptions.ts
+++ packages/cf-worker/src/generators/application/lib/normaliseOptions.ts
@@ -10,44 +10,30 @@ import { Schema } from '../schema';
 
 export interface NormalisedSchema extends Schema {
   projectName: string;
-  appProjectRoot: string;
-  e2eProjectName: string;
-  e2eProjectRoot: string;
+  projectRoot: string;
+  projectDirectory: string;
   parsedTags: string[];
-  fileName: string;
-  js?: boolean;
 }
 
 export function normaliseOptions(
   host: Tree,
   options: Schema
 ): NormalisedSchema {
-  const appDirectory = options.directory
-    ? `${names(options.directory).fileName}/${names(options.name).fileName}`
-    : names(options.name).fileName;
-
-  const { appsDir } = getWorkspaceLayout(host);
-
-  const appProjectName = appDirectory.replace(new RegExp('/', 'g'), '-');
-  const e2eProjectName = `${appProjectName}-e2e`;
-
-  const appProjectRoot = joinPathFragments(appsDir, appDirectory);
-  const e2eProjectRoot = joinPathFragments(appsDir, `${appDirectory}-e2e`);
-
+  const name = names(options.name).fileName;
+  const projectDirectory = options.directory
+    ? `${names(options.directory).fileName}/${name}`
+    : name;
+  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const projectRoot = `${getWorkspaceLayout(host).appsDir}/${projectDirectory}`;
   const parsedTags = options.tags
     ? options.tags.split(',').map((s) => s.trim())
     : [];
 
-  const fileName = 'index';
-
   return {
     ...options,
-    name: names(options.name).fileName,
-    projectName: appProjectName,
-    appProjectRoot,
-    e2eProjectRoot,
-    e2eProjectName,
+    projectName,
+    projectRoot,
+    projectDirectory,
     parsedTags,
-    fileName,
   };
 }
