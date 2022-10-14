ba4481355e41c110d06f9692f96b0f61250ed70c
Elias Cerne <eliascerne@icloud.com>
2022-10-14 08:01:24 +0200

    feat: add functions to application

--- packages/cf-worker/src/generators/application/application.ts
+++ packages/cf-worker/src/generators/application/application.ts
@@ -15,19 +15,57 @@ import { normaliseOptions, NormalisedSchema } from './lib/normaliseOptions';
 import { initGenerator } from '../init/init';
 import { addProjectCfg } from './lib/addProjectCfg';
 import { setDefaults } from './lib/setDefaults';
-import { addFiles } from './lib/addFiles';
+//import { addFiles } from './lib/addFiles';
+
+function normalizeOptions(
+  tree: Tree,
+  options: ApplicationSchema
+): NormalisedSchema {
+  const name = names(options.name).fileName;
+  const projectDirectory = options.directory
+    ? `${names(options.directory).fileName}/${name}`
+    : name;
+  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const projectRoot = `${getWorkspaceLayout(tree).appsDir}/${projectDirectory}`;
+  const parsedTags = options.tags
+    ? options.tags.split(',').map((s) => s.trim())
+    : [];
+
+  return {
+    ...options,
+    projectName,
+    projectRoot,
+    projectDirectory,
+    parsedTags,
+  };
+}
+
+function addFiles(tree: Tree, options: NormalisedSchema) {
+  const templateOptions = {
+    ...options,
+    ...names(options.name),
+    offsetFromRoot: offsetFromRoot(options.projectRoot),
+    template: '',
+  };
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions
+  );
+}
 
 export default async function applicationGenerator(
   host: Tree,
   schema: ApplicationSchema
 ) {
-  const options = normaliseOptions(host, schema);
+  const options = normalizeOptions(host, schema);
 
   const initTask = await initGenerator(host, { ...options });
 
-  addFiles(host, options);
-
   addProjectCfg(host, options);
+
+  addFiles(host, options);
 
   setDefaults(host, options);
 
