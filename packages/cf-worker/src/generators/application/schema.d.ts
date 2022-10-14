57e523cb9eeb102103120ac6e27a2d362d1d2fcd
Elias Cerne <eliascerne@icloud.com>
2022-10-14 09:41:39 +0200

    feat: add template to supportedTemplates

--- packages/cf-worker/src/generators/application/schema.d.ts
+++ packages/cf-worker/src/generators/application/schema.d.ts
@@ -1,5 +1,8 @@
+import { SupportedTemplates } from '../../typings/SupportedTemplates';
+
 export interface ApplicationSchema {
   name: string;
+  template: SupportedTemplates;
   tags?: string;
   skipFormat?: boolean;
   directory?: string;
