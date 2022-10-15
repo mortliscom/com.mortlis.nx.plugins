29f8e2e10bfcdb812ba9ec14636a96c7f461533e
Elias Cerne <eliascerne@icloud.com>
2022-10-15 16:41:18 +0200

    feat: create new export in versions.ts

--- packages/cf-worker/src/utils/versions.ts
+++ packages/cf-worker/src/utils/versions.ts
@@ -5,3 +5,4 @@ export const wranglerVersion = '2.1.11';
 export const eslintConfigNextVersion = '12.3.1';
 export const babelPluginStyledComponentsVersion = '1.10.7';
 export const tsLibVersion = '^2.3.0';
+export const cloudflareWorkersTypes = '^3.17.0';
