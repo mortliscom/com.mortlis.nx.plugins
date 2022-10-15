cf74be2ed3a2bbfe285bca0c503f2c149896e371
Elias Cerne <eliascerne@icloud.com>
2022-10-15 19:18:48 +0200

    feat: create new function in file and replace with local one

--- packages/cf-worker/src/generators/application/application.ts
+++ packages/cf-worker/src/generators/application/application.ts
@@ -9,6 +9,8 @@ import {
 } from '@nrwl/devkit';
 import * as path from 'path';
 import { ApplicationGeneratorSchema } from './schema';
+import { normaliseOptions } from './lib/normaliseOptions';
+import { projectConfiguration } from './lib/projectConfiguration';
 
 interface NormalizedSchema extends ApplicationGeneratorSchema {
   projectName: string;
@@ -17,53 +19,29 @@ interface NormalizedSchema extends ApplicationGeneratorSchema {
   parsedTags: string[];
 }
 
-function normalizeOptions(tree: Tree, options: ApplicationGeneratorSchema): NormalizedSchema {
-  const name = names(options.name).fileName;
-  const projectDirectory = options.directory
-    ? `${names(options.directory).fileName}/${name}`
-    : name;
-  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
-  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
-  const parsedTags = options.tags
-    ? options.tags.split(',').map((s) => s.trim())
-    : [];
-
-  return {
-    ...options,
-    projectName,
-    projectRoot,
-    projectDirectory,
-    parsedTags,
-  };
-}
-
 function addFiles(tree: Tree, options: NormalizedSchema) {
-    const templateOptions = {
-      ...options,
-      ...names(options.name),
-      offsetFromRoot: offsetFromRoot(options.projectRoot),
-      template: ''
-    };
-    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
+  const templateOptions = {
+    ...options,
+    ...names(options.name),
+    offsetFromRoot: offsetFromRoot(options.projectRoot),
+    template: '',
+  };
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions
+  );
 }
 
-export default async function (tree: Tree, options: ApplicationGeneratorSchema) {
-  const normalizedOptions = normalizeOptions(tree, options);
-  addProjectConfiguration(
-    tree,
-    normalizedOptions.projectName,
-    {
-      root: normalizedOptions.projectRoot,
-      projectType: 'library',
-      sourceRoot: `${normalizedOptions.projectRoot}/src`,
-      targets: {
-        build: {
-          executor: "@com.mortlis.nx.plugins/cf-worker:build",
-        },
-      },
-      tags: normalizedOptions.parsedTags,
-    }
-  );
+export default async function (
+  tree: Tree,
+  options: ApplicationGeneratorSchema
+) {
+  const normalisedOptions = normaliseOptions(tree, options);
+  
+  projectConfiguration(tree, normalisedOptions);
+
   addFiles(tree, normalizedOptions);
   await formatFiles(tree);
 }
