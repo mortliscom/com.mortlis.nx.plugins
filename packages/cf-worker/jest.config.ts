13f032c452362dad17a997e854d465c4d88f936a
Elias Cerne <eliascerne@icloud.com>
2022-10-15 21:08:53 +0200

    create: new plugin workspace cause nx docs suck (dont tell anyone)

---
+++ packages/cf-worker/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'cf-worker',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/cf-worker',
+};
