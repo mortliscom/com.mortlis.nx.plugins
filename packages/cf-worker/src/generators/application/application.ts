7570cc28fbf5f6a7ad1c3c48876264d43d0d4e71
Elias Cerne <eliascerne@icloud.com>
2022-10-15 19:22:35 +0200

    feat: replace addFiles local function with extern

--- packages/cf-worker/src/generators/application/application.ts
+++ packages/cf-worker/src/generators/application/application.ts
@@ -11,6 +11,7 @@ import * as path from 'path';
 import { ApplicationGeneratorSchema } from './schema';
 import { normaliseOptions } from './lib/normaliseOptions';
 import { projectConfiguration } from './lib/projectConfiguration';
+import { addFiles } from './lib/addFiles';
 
 interface NormalizedSchema extends ApplicationGeneratorSchema {
   projectName: string;
@@ -19,29 +20,15 @@ interface NormalizedSchema extends ApplicationGeneratorSchema {
   parsedTags: string[];
 }
 
-function addFiles(tree: Tree, options: NormalizedSchema) {
-  const templateOptions = {
-    ...options,
-    ...names(options.name),
-    offsetFromRoot: offsetFromRoot(options.projectRoot),
-    template: '',
-  };
-  generateFiles(
-    tree,
-    path.join(__dirname, 'files'),
-    options.projectRoot,
-    templateOptions
-  );
-}
-
 export default async function (
   tree: Tree,
   options: ApplicationGeneratorSchema
 ) {
   const normalisedOptions = normaliseOptions(tree, options);
-  
+
   projectConfiguration(tree, normalisedOptions);
 
-  addFiles(tree, normalizedOptions);
+  addFiles(tree, normalisedOptions);
+  
   await formatFiles(tree);
 }
