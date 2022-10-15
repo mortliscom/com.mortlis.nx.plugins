8457b49b829217b2b2a993e648845e2c62e637b3
Elias Cerne <eliascerne@icloud.com>
2022-10-15 16:13:20 +0200

    feat: write content to files

--- packages/cf-worker/src/generators/application/lib/wranglerInit.ts
+++ packages/cf-worker/src/generators/application/lib/wranglerInit.ts
@@ -1,6 +1,15 @@
 import { Tree } from '@nrwl/devkit';
 import { NormalisedSchema } from './normaliseOptions';
+import { tsconfigContent } from './filesContent';
 
-export function wranglerInit(tree: Tree, options: NormalisedSchema) {
-    return;
+export function wranglerInit(host: Tree, options: NormalisedSchema) {
+  let wranglerConfigContent = `name = "${options.projectName}"\n`;
+  wranglerConfigContent += `compatibility_date = "${new Date()
+    .toISOString()
+    .substring(0, 10)}"\n`;
+
+  host.write(`${options.projectRoot}/wrangler.toml`, wranglerConfigContent);
+  host.write(`${options.projectRoot}/tsconfig.json`, tsconfigContent);
+
+  return;
 }
