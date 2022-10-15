7f5f9d29862bc161951026c6704580e9a8ca358e
Elias Cerne <eliascerne@icloud.com>
2022-10-15 19:47:45 +0200

    feat: change to appDir instead of lib dir

--- e2e/cf-worker-e2e/tests/cf-worker.spec.ts
+++ e2e/cf-worker-e2e/tests/cf-worker.spec.ts
@@ -29,7 +29,7 @@ describe('cf-worker e2e', () => {
   it('should create cf-worker', async () => {
     const project = uniq('cf-worker');
     await runNxCommandAsync(
-      `generate @com.mortlis.nx.plugins/cf-worker:cf-worker ${project}`
+      `generate @com.mortlis.nx.plugins/cf-worker:application ${project}`
     );
     const result = await runNxCommandAsync(`build ${project}`);
     expect(result.stdout).toContain('Executor ran');
@@ -39,7 +39,7 @@ describe('cf-worker e2e', () => {
     it('should create src in the specified directory', async () => {
       const project = uniq('cf-worker');
       await runNxCommandAsync(
-        `generate @com.mortlis.nx.plugins/cf-worker:cf-worker ${project} --directory subdir`
+        `generate @com.mortlis.nx.plugins/cf-worker:application ${project} --directory subdir`
       );
       expect(() =>
         checkFilesExist(`apps/subdir/${project}/src/index.ts`)
@@ -55,7 +55,7 @@ describe('cf-worker e2e', () => {
         'dist/packages/cf-worker'
       );
       await runNxCommandAsync(
-        `generate @com.mortlis.nx.plugins/cf-worker:cf-worker ${projectName} --tags e2etag,e2ePackage`
+        `generate @com.mortlis.nx.plugins/cf-worker:application ${projectName} --tags e2etag,e2ePackage`
       );
       const project = readJson(`apps/${projectName}/project.json`);
       expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
