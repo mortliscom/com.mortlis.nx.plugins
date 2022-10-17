cfa7ecf6e6fb4c4823bd1e7b34e950c1b8223651
Elias Cerne <eliascerne@icloud.com>
2022-10-17 19:48:56 +0200

    feat: add read and write logic to initWrangler

--- packages/cf-worker/src/generators/application/lib/initWrangler.ts
+++ packages/cf-worker/src/generators/application/lib/initWrangler.ts
@@ -1,6 +1,5 @@
 import { Tree } from '@nrwl/devkit';
 import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
-import { spawn } from 'child_process';
 import { NormalisedSchema } from '../typings/NormalisedSchema';
 
 export function initWrangler(tree: Tree, options: NormalisedSchema) {
@@ -8,9 +7,33 @@ export function initWrangler(tree: Tree, options: NormalisedSchema) {
   wranglerTomlContent += `compatibility_date = "${new Date()
     .toISOString()
     .substring(0, 10)}"`;
-  let tsConfigContent = tree.read('packages/cf-worker/src/generators/application/utils/tsconfig.json', 'utf-8');
-  
-  let indexTemplate = "";
+  const tsConfigContent = tree.read(
+    './packages/cf-worker/src/generators/application/utils/files/tsconfig.json.template'
+  );
 
-  if (options.)
+  let indexJsTemplate = '';
+
+  // switch (options.template) {
+  //   case 'fetch-handler':
+  //     indexJsTemplate = tree.read(
+  //       'packages/cf-worker/src/generators/application/utils/files/fetchHandler.ts.template',
+  //       'utf-8'
+  //     );
+  //     break;
+  //   case 'scheduled-handler':
+  //     indexJsTemplate = tree.read(
+  //       'packages/cf-worker/src/generators/application/utils/files/scheduledHandler.ts.template',
+  //       'utf-8'
+  //     );
+  //     break;
+  //   default:
+  //     break;
+  // }
+
+  console.log(tsConfigContent);
+
+  tree.write(`${options.projectRoot}/wrangler.toml`, wranglerTomlContent);
+  // Fix: Does not write to tsconfig; tsconfigocontent reader is broken; wrong path persumaby
+  tree.write(`${options.projectRoot}/tsconfig.json`, tsConfigContent);
+  //tree.write(`${options.projectRoot}/src/index.ts`, indexJsTemplate);
 }
