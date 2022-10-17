5f11c960911a24da0b1cd42d07a2bd17dfb0c744
Elias Cerne <eliascerne@icloud.com>
2022-10-17 18:15:25 +0200

    feat: initWrangler functionality

--- packages/cf-worker/src/generators/application/lib/initWrangler.ts
+++ packages/cf-worker/src/generators/application/lib/initWrangler.ts
@@ -1,1 +1,16 @@
-export function initWrangler() {}
+import { Tree } from '@nrwl/devkit';
+import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
+import { spawn } from 'child_process';
+import { NormalisedSchema } from '../typings/NormalisedSchema';
+
+export function initWrangler(tree: Tree, options: NormalisedSchema) {
+  let wranglerTomlContent = `name = ${options.name}\n`;
+  wranglerTomlContent += `compatibility_date = "${new Date()
+    .toISOString()
+    .substring(0, 10)}"`;
+  let tsConfigContent = tree.read('packages/cf-worker/src/generators/application/utils/tsconfig.json', 'utf-8');
+  
+  let indexTemplate = "";
+
+  if (options.)
+}
