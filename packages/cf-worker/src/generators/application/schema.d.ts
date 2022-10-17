79a7c681652522a5b91a1a0206081c1192733f1d
Elias Cerne <eliascerne@icloud.com>
2022-10-17 19:47:46 +0200

    feat: add template to schema

--- packages/cf-worker/src/generators/application/schema.d.ts
+++ packages/cf-worker/src/generators/application/schema.d.ts
@@ -1,5 +1,8 @@
+import { TemplateOptions } from './typings/templateOptions';
+
 export interface ApplicationGeneratorSchema {
-    name: string;
-    tags?: string;
-    directory?: string;
+  name: string;
+  tags?: string;
+  directory?: string;
+  template?: TemplateOptions;
 }
